05eb99619ee033a4cd86eb218f89086c98dd8718
Andres Soria <andres.zov@gmail.com>
2024-06-19 12:42:31 -0600

    Add a new namespace with 29541 for node list elements

--- js/view.js
+++ js/view.js
@@ -1,16 +1,19 @@
 export default class View {
   $ = {};
+  $$ = {};
+
   constructor() {
     this.$.menu = this.#qs('[data-id="menu"]');
     this.$.menuItems = this.#qs('[data-id="menu-items"]');
     this.$.menuBtn = this.#qs('[data-id="menu-button"');
     this.$.resetBtn = this.#qs('[data-id="reset-btn"]');
     this.$.newRoundBtn = this.#qs('[data-id="new-round-btn"]');
-    this.$.squares = this.#qsAll('[data-id="square"]');
     this.$.modal = this.#qs('[data-id="modal"]');
     this.$.modalText = this.#qs('[data-id="modal-text"]');
     this.$.modalBtn = this.#qs('[data-id="modal-btn"]');
     this.$.turn = this.#qs('[data-id="turn"]');
+
+    this.$$.squares = this.#qsAll('[data-id="square"]');
 
     //UI only event listener
     this.$.menuBtn.addEventListener('click', () => {
