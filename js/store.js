3dc253453b0aa16302708a4be9aa56a2048a13de
Andres Soria <andres.zov@gmail.com>
2024-08-07 20:19:45 -0600

    Create logic to check the status of the game when completed

--- js/store.js
+++ js/store.js
@@ -14,7 +14,39 @@ export default class Store {
 
     const currentPlayer = this.players[state.moves.length % 2];
 
-    return { moves: state.moves, currentPlayer };
+    const winningPatterns = [
+      [1, 2, 3],
+      [1, 5, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 5, 7],
+      [3, 6, 9],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+
+    let winner = null;
+
+    for (const player of this.players) {
+      const selectedSquareIds = state.moves
+        .filter((move) => move.player.id === player.id)
+        .map((move) => move.squareId);
+
+      for (const pattern of winningPatterns) {
+        if (pattern.every((v) => selectedSquareIds.includes(v))) {
+          winner = player;
+        }
+      }
+    }
+
+    return {
+      moves: state.moves,
+      currentPlayer,
+      status: {
+        isComplete: winner != null || state.moves.length === 9,
+        winner,
+      },
+    };
   }
 
   playerMove(squareId) {
